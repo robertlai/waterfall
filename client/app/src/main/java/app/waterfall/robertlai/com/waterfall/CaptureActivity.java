097ae59857b42deae4a884f29c45889cd19692a4
Robert Chun Yan Lai <rlai816@gmail.com>
2015-09-17 22:24:17 -0400

    Fixed file paths, wrote GET method

--- client/app/src/main/java/app/waterfall/robertlai/com/waterfall/CaptureActivity.java
+++ client/app/src/main/java/app/waterfall/robertlai/com/waterfall/CaptureActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.Looper;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -16,7 +18,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.loopj.android.http.*;
 
@@ -25,6 +26,8 @@ import org.apache.http.Header;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class CaptureActivity extends ActionBarActivity {
@@ -36,91 +39,143 @@ public class CaptureActivity extends ActionBarActivity {
     private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
     private static Date date;
     private static Uri imageUri;
+    private static boolean hasNext = true;
+    Timer timer;
+
+    public void startTimer(int seconds){
+        timer = new Timer();
+        timer.schedule(new getPhotoTask(), seconds*1000);
+    }
+
+    class getPhotoTask extends TimerTask {
+        public void run() {
+            Looper.prepare();
+            getPhoto();
+            startTimer(5);
+            Looper.loop();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
 
-        Button cameraButton = (Button)findViewById(R.id.button_camera);
+        Button cameraButton = (Button) findViewById(R.id.button_camera);
         cameraButton.setOnClickListener(cameraListener);
 
         getPhoto();
+        //startTimer(5);
     }
 
     private OnClickListener cameraListener = new OnClickListener() {
-        public void onClick(View view){
+        public void onClick(View view) {
             takePhoto(view);
         }
     };
 
-    private void takePhoto(View view){
+    private void takePhoto(View view) {
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
         date = new Date();
-        File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES + "/Waterfall"), "wf_" + sdf.format(date) + ".jpg");
+        File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES + "/Waterfall"), "wf_temp.jpg");
         imageUri = Uri.fromFile(photo);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
         startActivityForResult(intent, TAKE_PICTURE);
         setIntent(null);
     }
 
-    private void getPhoto(){
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(URL, new FileAsyncHttpResponseHandler(this) {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, File response) {
-                // Do something with the file `response`
-                System.out.println(statusCode + " " + response.getAbsolutePath());
-            }
+    private void getPhoto() {
+        try {
+            AsyncHttpClient client = new AsyncHttpClient();
+            client.get(URL, new FileAsyncHttpResponseHandler(this) {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, File response) {
+                    // Do something with the file `response`
+                    System.out.println(statusCode + " " + response.getAbsolutePath());
+                    ImageView imageView = (ImageView) findViewById(R.id.image_camera);
+                    Bitmap bitmap = BitmapFactory.decodeFile(response.getPath());
+                    int newHeight = (int) (bitmap.getHeight() * ((float) SCALED_WIDTH / bitmap.getWidth()));
+                    Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, SCALED_WIDTH, newHeight, true);
+                    imageView.setImageBitmap(scaledBitmap);
+                }
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable e, File response) {
-                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                System.out.println(statusCode);
-            }
-        });
+                @Override
+                public void onFailure(int statusCode, Header[] headers, Throwable e, File response) {
+                    // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                    System.out.println(statusCode);
+                    hasNext = false;
+                }
+            });
+        } catch (Exception e){
+
+        }
     }
 
-    private void postPhoto(){
+    private void postPhoto() {
         File myFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES + "/Waterfall"), "wf_temp.jpg");
         RequestParams params = new RequestParams();
         try {
             params.put("image", myFile);
-        } catch(Exception e) {}
+        } catch (Exception e) {
+        }
 
         AsyncHttpClient client = new AsyncHttpClient();
         client.post(URL, params, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] bytes) {
+                //Toast.makeText(CaptureActivity.this, statusCode, Toast.LENGTH_LONG).show();
                 System.out.println(statusCode);
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] bytes, Throwable throwable) {
+                //Toast.makeText(CaptureActivity.this, statusCode, Toast.LENGTH_LONG).show();
                 System.out.println(statusCode);
             }
         });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
 
         if (resultCode == Activity.RESULT_OK) {
             Uri selectedImage = imageUri;
             getContentResolver().notifyChange(selectedImage, null);
 
-            ImageView imageView = (ImageView)findViewById(R.id.image_camera);
+            ImageView imageView = (ImageView) findViewById(R.id.image_camera);
             ContentResolver cr = getContentResolver();
             Bitmap bitmap;
 
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(cr, selectedImage);
-                int newHeight = (int) ( bitmap.getHeight() * ((float)SCALED_WIDTH / bitmap.getWidth()) );
+                int newHeight = (int) (bitmap.getHeight() * ((float) SCALED_WIDTH / bitmap.getWidth()));
                 Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, SCALED_WIDTH, newHeight, true);
                 imageView.setImageBitmap(scaledBitmap);
-                Toast.makeText(CaptureActivity.this, selectedImage.toString(), Toast.LENGTH_LONG).show();
-            } catch (Exception e){
+                //Toast.makeText(CaptureActivity.this, selectedImage.toString(), Toast.LENGTH_LONG).show();
+                //postPhoto();
+                File myFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES + "/Waterfall"), "wf_temp.jpg");
+                RequestParams params = new RequestParams();
+                try {
+                    params.put("image", myFile);
+                } catch (Exception e) {
+                }
+
+                AsyncHttpClient client = new AsyncHttpClient();
+                client.post(URL, params, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] bytes) {
+                        //Toast.makeText(CaptureActivity.this, statusCode, Toast.LENGTH_LONG).show();
+                        System.out.println(statusCode);
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] bytes, Throwable throwable) {
+                        //Toast.makeText(CaptureActivity.this, statusCode, Toast.LENGTH_LONG).show();
+                        System.out.println(statusCode);
+                    }
+                });
+            } catch (Exception e) {
                 Log.e(logtag, e.toString());
             }
         }
